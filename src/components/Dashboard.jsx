8a49c7e716d30ddbc7e04b565ffdd2d55a3df444
Bhargavi-hub154 <2500030127@kluniversity.in>
2026-02-22 10:27:38 +0530

    first

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,26 @@
+import Header from "./Header";
+import UserCard from "./UserCard";  
+
+function Dashboard() {
+  const user = {
+    name: "Bhargavi",
+    email: "bhargavi@gmail.com",
+    role: "Student",
+    phone: "+91 9876543210",
+    location: "Andhra Pradesh",
+    course: "BTech CSE (AI & ML)",
+    joined: "2025",
+    status: "Active",
+    about: "Passionate about AI and building real-world projects.",
+    skills: ["HTML", "CSS", "Professional in JavaScript", "React", "Master in Java"]
+  };
+
+  return (
+    <div>
+      <Header title="User Dashboard" />
+      <UserCard user={user} />
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ src/components/Usercard.jsx
@@ -0,0 +1,34 @@
+function UserCard({ user }) {
+  return (
+    <div className="user-card">
+
+      <img
+        src="https://i.pinimg.com/736x/1a/98/9f/1a989fdeb54d8a7ba2f412d67f73c8ef.jpg"
+        alt="profile"
+        className="profile-image"
+      />
+
+      <h3>{user.name}</h3>
+      <p><strong>Status:</strong> {user.status}</p>
+      <p><strong>Email:</strong> {user.email}</p>
+      <p><strong>Phone:</strong> {user.phone}</p>
+      <p><strong>Location:</strong> {user.location}</p>
+      <p><strong>Course:</strong> {user.course}</p>
+      <p><strong>Joined:</strong> {user.joined}</p>
+
+      <p><strong>About:</strong> {user.about}</p>
+
+      <div>
+        <strong>Skills:</strong>
+        <ul>
+          {user.skills.map((skill, index) => (
+            <li key={index}>{skill}</li>
+          ))}
+        </ul>
+      </div>
+
+    </div>
+  );
+}
+
+export default UserCard;
